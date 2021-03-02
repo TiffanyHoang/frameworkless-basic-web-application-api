4c05f3f5615187d1c9e56a6245b6f507ee40ee0f
Tiffany Hoang <tiffany.hoang@myob.com>
2021-03-02 11:47:00 +1100

    update Greeting test

--- WebApplication_Tests/RequestHandler_Test.cs
+++ WebApplication_Tests/RequestHandler_Test.cs
@@ -6,22 +6,28 @@ namespace WebApplication_Tests
     public class RequestHandler_Test
     {
         private readonly Repository _repository;
-        private readonly string responseString;
+        private readonly RequestHandler _requestHandler;
         public RequestHandler_Test()
         {
             _repository = new Repository();
+            _requestHandler = new RequestHandler(_repository);
         }
 
         [Fact]
         public void Greeting_ReturnCorrectGreetingMessage()
         {
-            var requestHandler = new RequestHandler(_repository);
-            var responseString = requestHandler.Greeting();
+            var responseString = _requestHandler.Greeting();
             var time = DateTime.Now.ToString("HH:mm");
             var date = DateTime.Now.ToString("dd MMM yyyy");
             var expected = $"Hello Tiffany - the time on the server is {time} on {date}";
 
             Assert.Equal(expected, responseString);
         }
+
+        // [Fact]
+        // public void GetPeopleList_ReturnCorrectListofPeopleInJsonFormat()
+        // {
+
+        // }
     }
 }
