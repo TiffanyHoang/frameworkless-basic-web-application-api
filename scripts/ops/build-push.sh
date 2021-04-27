feee86c64daa960a896f2dd34716a1db812091e4
Tiffany Hoang <tiffany.hoang@myob.com>
2021-04-27 16:06:16 +1000

    add unit-tests.sh

--- scripts/ops/build-push.sh
+++ scripts/ops/build-push.sh
@@ -1,7 +1,7 @@
-echo 'Build & push app to ECR'
-
+#!/usr/bin/env bash
+echo ':docker: Build app image'
 docker build -f Dockerfile -t 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany .
-
+echo ':docker: Push app image to ECR'
 docker push 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany
-
+echo ':docker: Remove app image'
 docker rmi 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany -f
