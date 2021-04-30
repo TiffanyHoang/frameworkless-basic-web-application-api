cc7f2398d70a028445f239b8adc46e6e764d1163
Tiffany Hoang <tiffany.hoang@myob.com>
2021-04-30 12:41:23 +1000

    edit dockerfile and add docker compse file

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,14 @@ WORKDIR /app
 COPY WebApplication ./
 RUN dotnet publish -c Release -o out
 
-FROM mcr.microsoft.com/dotnet/aspnet:5.0
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS run
 WORKDIR /app
 COPY --from=build /app/out .
 ENV COMPlus_EnableDiagnostics=0
 CMD ["WebApplication.dll"]
-ENTRYPOINT ["dotnet"]
+ENTRYPOINT ["dotnet"] 
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS test
+WORKDIR /app
+COPY . ./
+RUN dotnet test
--- scripts/ops/build-push.sh
+++ scripts/ops/build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo '--- :docker: Build app image'
-docker build -f Dockerfile -t 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany .
+docker build --target run -t 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany .
 echo '--- :docker: Push app image to ECR'
 docker push 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany
 echo '--- :docker: Remove app image'
