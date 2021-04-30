af194c491aa9bf7152f6a65b4f84d0a775b67cbd
Tiffany Hoang <tiffany.hoang@myob.com>
2021-04-30 13:24:37 +1000

    using docker-compose in test & build sh

--- scripts/ops/build-push.sh
+++ scripts/ops/build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo '--- :docker: Build app image'
-docker build --target run -t 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany .
+docker-compose build app
 echo '--- :docker: Push app image to ECR'
-docker push 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany
+docker-compose push app
 echo '--- :docker: Remove app image'
-docker rmi 138666658526.dkr.ecr.ap-southeast-2.amazonaws.com/frameworkless-basic-web-app-tiffany -f
+docker-compose down --rmi all
--- scripts/ops/unit-tests.sh
+++ scripts/ops/unit-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo '--- :docker: Build unit tests image'
-docker build --target test -t test .
+docker-compose build test
 echo '--- :docker: Remove unit tests image'
-docker rmi test -f
+docker-compose down --rmi all
