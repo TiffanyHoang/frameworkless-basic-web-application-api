ee2e37777c433d44a6b5b3eb38d187a708ab0dd9
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-03 11:54:40 +1000

    change order of prepare repo step in sh

--- ops/bin/build-push-to-ecr.sh
+++ ops/bin/build-push-to-ecr.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-echo '--- :docker: Build app image'
-docker-compose build app
 echo '--- :ecr: Prepare repo in ECR'
 aws cloudformation deploy --template-file ./aws/ecr-template.yaml --stack-name tiffany-ecr --region ap-southeast-2 
+echo '--- :docker: Build app image'
+docker-compose build app
 echo '--- :docker: Push app image to ECR'
 docker-compose push app
 echo '--- :docker: Remove app image'
