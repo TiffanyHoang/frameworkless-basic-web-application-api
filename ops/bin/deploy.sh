3e8f06c15fdead162e7a1d2d0a37c159bd3e419c
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-03 11:48:40 +1000

    update image name, add ecr cloudformation

--- ops/bin/deploy.sh
+++ ops/bin/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo '--- :amazon-ecs: Deploy on AWS ECS'
-aws ecs --no-cli-pager update-service --cluster tiffany-cluster --service tiffany-service --force-new-deployment
+aws ecs --no-cli-pager update-service --cluster tiffany-cluster --service tiffany-service --task-definition tiffany-task:3 --force-new-deployment
