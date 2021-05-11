46f97be7ffc91fea74352d2f98d41c5b55cd72ca
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-11 14:53:44 +1000

    add version for image

--- ops/bin/build-push-to-ecr.sh
+++ ops/bin/build-push-to-ecr.sh
@@ -2,8 +2,8 @@
 echo '--- :ecr: Prepare repo in ECR'
 aws cloudformation deploy --template-file ./aws/ecr-template.yaml --stack-name tiffany-ecr --region ap-southeast-2 
 echo '--- :docker: Build app image'
-docker-compose build app
+version=$(git describe --tags --always) docker-compose build app
 echo '--- :docker: Push app image to ECR'
-docker-compose push app
+version=$(git describe --tags --always) docker-compose push app
 echo '--- :docker: Remove app image'
 docker-compose down --rmi all
