750cdb4b3e835fd9d58d6cf6611ff3b34554cfbb
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-11 11:33:05 +1000

    update deploy step using cloudformation

--- ops/bin/deploy.sh
+++ ops/bin/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo '--- :amazon-ecs: Deploy on AWS ECS'
-aws ecs --no-cli-pager update-service --cluster tiffany-cluster --service tiffany-service --task-definition tiffany-task:3 --force-new-deployment
+echo '--- :amazon: Deploy on AWS ECS'
+aws cloudformation deploy --template-file ./aws/deployment-template.yaml --stack-name tiffany-deployment-stack --region ap-southeast-2
