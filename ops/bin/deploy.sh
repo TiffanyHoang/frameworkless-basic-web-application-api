054f1bc72bc1e5a87073f5f6b1a9a42ea03cefc7
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-11 15:32:52 +1000

    add version for image

--- ops/bin/deploy.sh
+++ ops/bin/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo '--- :amazon: Deploy on AWS ECS'
-aws cloudformation deploy --template-file ./aws/deployment-template.yaml --stack-name tiffany-deployment-stack --region ap-southeast-2
+aws cloudformation deploy --template-file ./aws/deployment-template.yaml  --parameter-overrides version=$(git describe --tags --always) --stack-name tiffany-deployment-stack --region ap-southeast-2
