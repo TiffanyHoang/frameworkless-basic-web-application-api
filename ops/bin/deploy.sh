ae8e7afcdb366757812c3b7f7ec518a0159cdcce
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-13 11:32:32 +1000

    stored config deployment as var

--- ops/bin/deploy.sh
+++ ops/bin/deploy.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 echo '--- :aws: Deploy on AWS ECS'
+commit=$(git describe --tags --always)
+vpcId='vpc-d88094bf'
+subnet1='subnet-0ecc2546'
+subnet2='subnet-316e9257'
 aws cloudformation deploy \
     --template-file ./aws/deployment-template.yaml \
-    --parameter-overrides version=$(git describe --tags --always) \
+    --parameter-overrides \
+        version=$commit \
+        vpcId=$vpcId \
+        subnet1=$subnet1 \
+        subnet2=$subnet2 \
     --stack-name tiffany-deployment-stack \
     --region ap-southeast-2
