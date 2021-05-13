229f8b4881d6c0438d41f1f2cdeb86354eeb53e9
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-13 13:47:51 +1000

    use var for ecr acc

--- ops/bin/build-push-to-ecr-dev.sh
+++ ops/bin/build-push-to-ecr-dev.sh
@@ -5,8 +5,8 @@ aws cloudformation deploy \
     --stack-name tiffany-ecr \
     --region ap-southeast-2     
 echo '--- :docker: Build app image'
-version=$(git describe --tags --always) docker-compose build app
+version=$(git describe --tags --always) ecrAcc='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com' docker-compose build app
 echo '--- :docker: Push app image to ECR'
-version=$(git describe --tags --always) docker-compose push app
+version=$(git describe --tags --always) ecrAcc='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com' docker-compose push app
 echo '--- :docker: Remove app image'
 docker-compose down --rmi all
