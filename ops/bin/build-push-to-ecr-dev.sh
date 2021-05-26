3f8f22bfae43116f807d4dbec6fa180362b71ccf
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-26 20:10:53 +1000

    created variables for build push image in script

--- ops/bin/build-push-to-ecr-dev.sh
+++ ops/bin/build-push-to-ecr-dev.sh
@@ -2,8 +2,10 @@
 set -eou pipefail
 set -o xtrace
 echo '--- :docker: Build app image'
-version=$(git describe --tags --always) ecrAcc='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com' docker-compose build app
+VERSION=$(git describe --tags --always) 
+ECRACC='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com'
+version=$VERSION ecrAcc=$ECRACC docker-compose build app
 echo '--- :docker: Push app image to ECR'
-version=$(git describe --tags --always) ecrAcc='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com' docker-compose push app
+version=$VERSION ecrAcc=$ECRACC docker-compose push app
 echo '--- :docker: Remove app image'
-version=$(git describe --tags --always) ecrAcc='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com' docker-compose down --rmi all
+version=$VERSION ecrAcc=$ECRACC docker-compose down --rmi all
