9552709dcf7beaffffa958adbb7a1def6136e623
Tiffany Hoang <tiffany.hoang@myob.com>
2021-05-27 10:12:27 +1000

    try annotation

--- ops/bin/buildkite-annotation.sh
+++ ops/bin/buildkite-annotation.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-url="tiffany-prod.fma.lab.myobdev.com"
-msg <<- @
-<h4>Endpoint:</h4>
-<a href="http://$url">$url</a>
-@
-buildkite-agent annotate "$msg" --style "success" \
-    --context "endpoint" 
+url='tiffany-prod.fma.lab.myobdev.com'
+buildkite-agent annotate "$url 🚀" --style 'success' --context 'ctx-success'
