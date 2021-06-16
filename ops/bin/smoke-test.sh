51bb4a6cde37483c96e003e8a1d8522137ae8bd2
Tiffany Hoang <tiffany.hoang@myob.com>
2021-06-16 15:48:24 +1000

    Smoke test (#17)
    * prod endpoint test in script & updated authentication on greeting handler
    
    * add smoke test in pipeline
    
    * added docker for smoke test script

--- ops/bin/smoke-test.sh
+++ ops/bin/smoke-test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eou pipefail
+echo '--- :docker: Build test image'
+docker build -f Dockerfile.test -t test .
 echo '--- :sunglasses: Prod endpoints test'
-SECRET=$(aws ssm get-parameter --name "tiffany-app-secret" --query Parameter.Value --output text --region ap-southeast-2) \
-ENDPOINT="https://tiffany-prod.fma.lab.myobdev.com" \
-dotnet test Smoke_Test
+SECRET=$(aws ssm get-parameter --name "tiffany-app-secret" --query Parameter.Value --output text --region ap-southeast-2) 
+docker run \
+-e SECRET=$SECRET \
+-e ENDPOINT="https://tiffany-prod.fma.lab.myobdev.com" \
+test Smoke_Test
+echo '--- :docker: Remove test image'
+docker rmi test -f 
+
